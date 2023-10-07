594bd77eaff5af25950aa3a6fcdf8dd6e78221bd
Baron Afutu <afutubaronny@gmail.com>
2023-10-07 13:52:35 +0000

    Updates 10-07-23

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,15 +1,136 @@
+const request = (endpoint, method, data = {}) => {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: `http://localhost:3000/api/v1/${endpoint}`,
+			type: method,
+			contentType: 'application/json',
+			data: data,
+			// beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + $('#token').val()); },
+			success: function (response) {
+				resolve(response)
+			},
+			error: function (error) {
+				alert('Error Connecting to Server!!!')
+				reject(error);
+			}
+		});
+	})
+}
+
+const makeListingCards = (listings) => {
+	let listingHtml="";
+	listings.forEach(element => {
+		listingHtml += `
+				<div class="col-lg-3 col-md-6">
+		      	<div class="item">
+		        <div class="position-absolute end-0 p-2"><i class="fa fa-heart fa-xl text-danger"></i></div>
+		        <a href="property-details.html#${element._id}"><img src="public/img/property-01.jpg" alt=""></a>
+		        <span class="category">Luxury Villa</span>
+		        <h6>GH¢ ${element.pricePerNight}</h6>
+		        <h4><a href="property-details.html">${element.title}</a></h4>
+		        <ul>
+		          <li>Bedrooms: <span>${element.bedrooms}</span></li>
+		          <li>Bathrooms: <span>${element.bathrooms}</span></li>
+		          <li>Area: <span>545m2</span></li>
+		          <li>Floor: <span>3</span></li>
+		          <li>Parking: <span>6 spots</span></li>
+		        </ul>
+		      </div>
+		    </div>`
+	});
+	return listingHtml;
+}
+
+$(document).ready(async function () {
+	// await request('listings', 'GET').then(data => {
+	// 	$("#listings").html(makeListingCards(data));
+	// }).catch(err => {
+	// 	console.log(err)
+	// })
+});
+
+
 (function ($) {
 
 	"use strict";
 
-	// Page loading animation
-	$(window).on('load', function () {
-		// $('#js-preloader').addClass('loaded');
-		$(document).ready(function() {
-			// $('.features').select2();
-			// $("#loginModal").modal('show');
-		});
+	$("#saveFilterBtn").click(async function (e) {
+		e.preventDefault();
+		let filters = {
+			"propertyType": $("#propertyType").val(),
+			"bedrooms": $("#bedrooms").val(),
+			"bathrooms": $("#bathrooms").val(),
+			"minPrice": $("#minPrice").val()|| 0,
+			"maxPrice": $("#maxPrice").val()|| 9999
+		}
+
+		await request('listings', 'GET', filters).then(data => {
+			if (data.length < 1) {
+				$("#listings").html("<p class='my-5 py-5 text-center h-75'>&nbsp;</p><p class='my-5 py-5 text-center h-75'>No listings match your request</p><p class='d-inline-block d-md-none my-5 py-5 text-center h-75'>&nbsp;</p>")
+				return;
+			}
+			$("#listings").html(makeListingCards(data));
+		}).catch(err => {
+			console.log(err)
+		})
 	});
+
+	$("#saveFilterFormBtn").click(async function (e) {
+		e.preventDefault();
+		// console.log("here")
+		const filtersForm = document.forms["filtersForm"];
+		const amenityList = filtersForm["amenities[]"];
+		let amenities=[];
+		for(let i=0;i<amenityList.length;i++){
+			if(amenityList[i].checked)
+				amenities.push(amenityList[i].value)
+		}
+		
+		$("#minPrice").val(filtersForm["minPrice"].value)
+		$("#maxPrice").val(filtersForm["maxPrice"].value)
+		$("#bedrooms").val(filtersForm["nBedrooms"].value)
+		$("#bathrooms").val(filtersForm["nBathrooms"].value)
+		$("#propertyType").val(filtersForm["propType"].value)
+
+		// console.log(filtersForm["amenities[]"]);
+		let filters = {
+			// "host": "",
+			// "title": "",
+			// "description": "",
+			"propertyType": filtersForm["propType"].value,
+			// "roomType": "",
+			"bedrooms": filtersForm["nBedrooms"].value,
+			"bathrooms": filtersForm["nBathrooms"].value,
+			"minPrice": filtersForm["minPrice"].value||0,
+			"maxPrice": filtersForm["maxPrice"].value||9999,
+			"amenities": amenities,
+			// "location": "",
+			// "coordiantes": "",
+			// "ghPostAddress": ""
+		}
+		console.log(filters)
+
+		await request('listings', 'GET', filters).then(data => {
+			if (data.length < 1) {
+				$("#listings").html("<p class='my-5 py-5 text-center h-75'>&nbsp;</p><p class='my-5 py-5 text-center h-75'>No listings match your request</p><hr><p class='my-5 py-5 text-center h-75'>&nbsp;</p>")
+				return;
+			}
+			$("#listings").html(makeListingCards(data));
+		}).catch(err => {
+			console.log(err)
+		})
+
+		$("#filtersModal").modal('hide');
+	});
+
+	// // Page loading animation
+	// $(window).on('load', function () {
+	// 	// $('#js-preloader').addClass('loaded');
+	// 	$(document).ready(function() {
+	// 		// $('.features').select2();
+	// 		// $("#loginModal").modal('show');
+	// 	});
+	// });
 
 
 	$(window).scroll(function () { // Scroll stuff
@@ -23,11 +144,11 @@
 		if (scroll >= box - header) {
 			$("header").addClass("background-header");
 		} else {
-			
+
 			$("header").removeClass("background-header");
 		}
-		if(properties>2500){
-			properties+=500;
+		if (properties > 2500) {
+			properties += 250;
 		}
 		if (scroll >= biger - box - properties) {
 			// console.log(scroll,properties,header)
